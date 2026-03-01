77aa2e2213f3711eb48d6537fb436192189f5571
ScopeRicrit <wahyudiwijaya2309@gmail.com>
2026-03-01 12:48:51 +0800

    Pengomentaran dog.dart

--- lib/dog.dart
+++ lib/dog.dart
@@ -1,10 +1,14 @@
 class Dog {
-  final int? id;
+  // Properti-properti
+  final int? id; // Properti ini tidak wajib karena diberikan secara otomatis di database.
   final String name;
   final int age;
 
+  // Inisialisasi instansi
   Dog({this.id, required this.name, required this.age});
 
+  // Mengoutput data dog dalam format Map 
+  // yang dapat diterima sebagai data JSON.
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -13,7 +17,12 @@ class Dog {
     };
   }
 
+  // Mengoutput instansi Dog baru dengan data JSON.
   factory Dog.fromJson(Map<String, dynamic> json) {
+    // id dan age akan diperiksa tipe datanya masing-masing.
+    // Seandainya berupa String, ia akan dikonversi menjadi int terlebih dahulu.
+    //
+    // Seandainya juga name berupa Null, ia akan digantikan oleh String kosong. 
     return Dog(
       id: json['id'] is String ? int.parse(json['id']) : json['id'],
       name: json['name'] ?? '',
