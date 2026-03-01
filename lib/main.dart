a5d5cedc9d0aa02d638ab1471bdd6c99708c0a6e
ScopeRicrit <wahyudiwijaya2309@gmail.com>
2026-03-01 16:13:38 +0800

    Mengomentari main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dog_provider.dart';
 
 void main() {
   runApp(
+    // Membuat ChangeNotifier baru dengan DogProvider.
     ChangeNotifierProvider(
       create: (_) => DogProvider(),
       child: const MaterialApp(home: HomeScreen()),
@@ -21,6 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+    // Memperoleh data-data Dog dari dog_provider.dart setiap frame.
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Provider.of<DogProvider>(context, listen: false).fetchDogs();
     });
@@ -31,10 +33,11 @@ class _HomeScreenState extends State<HomeScreen> {
     final provider = Provider.of<DogProvider>(context);
     return Scaffold(
       appBar: AppBar(title: const Text('MySQL Dog Manager (Edge)')),
-      body: provider.isLoading
+      body: provider.isLoading // Menampilkan ikon loading selama provider memperoleh data Dog.
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
         itemCount: provider.dogs.length,
+        // Membuat kartu untuk setiap Dog yang tersedia 
         itemBuilder: (context, index) {
           final dog = provider.dogs[index];
           return ListTile(
@@ -47,6 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
           );
         },
       ),
+      // Memicu dialog penambahan Dog baru
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => _showDialog(context),
@@ -54,18 +58,21 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  // Dialog penambahan Dog baru
   void _showDialog(BuildContext context) {
-    final n = TextEditingController();
-    final a = TextEditingController();
+    final n = TextEditingController(); // name / Nama
+    final a = TextEditingController(); // age / Umur
     showDialog(
       context: context,
       builder: (_) => AlertDialog(
         title: const Text('Tambah Anjing'),
+        // Input teks nama dan input bilangan umur disusun secara kolom.  
         content: Column(mainAxisSize: MainAxisSize.min, children: [
           TextField(controller: n, decoration: const InputDecoration(labelText: 'Nama')),
           TextField(controller: a, decoration: const InputDecoration(labelText: 'Umur'), keyboardType: TextInputType.number),
         ]),
         actions: [
+          // Tombol yang memicu penyimpanan Dog baru
           ElevatedButton(
               onPressed: () {
                 Provider.of<DogProvider>(context, listen: false).insertDog(n.text, int.parse(a.text));
