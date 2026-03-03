3aa232392c340d247d0a86164997e00e84032b68
ScopeRicrit <wahyudiwijaya2309@gmail.com>
2026-03-03 23:07:34 +0800

    Mengomentari setiap line api_service.dart

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -11,41 +11,55 @@ import 'dog.dart';
 // Future akan langsung dibuat, lalu line yang menggunakan keyword await akan ditunggu sebelum lanjut.
 class ApiService {
   // Alamat API localhost
-  // Gunakan localhost jika running di Edge/Browser
+  // Tiap operasi API akan membuat sebuah objek URI (Uniform Resource Identifier) dengan base URL.
+  // Objek URI digunakan untuk mengakses semua resource dari alamat URI tersebut
   static const String baseUrl = 'http://localhost/flutter_api/dogs.php';
 
-  // Memperoleh semua data-data Dog menggunakan HTTP GET.
+  // Memperoleh semua data-data Dog.
   Future<List<Dog>> getDogs() async {
     try {
-      // Menjalankan suatu operasi HTTP.
+      // Menjalankan operasi HTTP GET.
       // Setelah selesai, server akan merespon dengan suatu kode status.
       final response = await http.get(Uri.parse(baseUrl));
 
-      // Melempar eror jika operasi API gagal.
       // Kode status 200 atau 'OK' merupakan kode umum untuk operasi API yang sukses.
       if (response.statusCode == 200) {
+        // Mengubah String response menjadi sebuah List yang terdiri atas map-map Dog.
         List<dynamic> data = json.decode(response.body);
+
+        // Membuat lalu mengoutput sebuah List baru yang terdiri atas instansi kelas Dog.
         return data.map((json) => Dog.fromJson(json)).toList();
       }
+      
+      // Jika operasi gagal, maka fungsi akan melempar eror.
       throw Exception('Gagal memuat data');
     } catch (e) {
+      // Eror yang dilempar dari blok try.
+      // Error: Gagal memuat data
       throw Exception('Error: $e');
     }
   }
 
   // Menambahkan satu Dog baru menggunakan HTTP POST.
-  Future<void> addDog(Dog dog) async {
+  Future<void> addDog(Dog dog) async
+    // Menjalankan operasi HTTP POST yang menambah suatu data kepada API.
     final response = await http.post(
       Uri.parse(baseUrl),
       headers: {'Content-Type': 'application/json'},
+      // Mengstruktur lalu mengkonversi data Dog menjadi data JSON.
       body: json.encode(dog.toJson()),
     );
+
+    // Jika operasi gagal, maka fungsi akan melempar eror.
     if (response.statusCode != 200) throw Exception('Gagal menambah data');
   }
 
   // Menghapus suatu Dog berdasarkan ID menggunakan HTTP DELETE.
   Future<void> deleteDog(int id) async {
+    // baseUrl ditambah juga sebuah query parameter ID untuk melacak target Dog.
     final response = await http.delete(Uri.parse('$baseUrl?id=$id'));
+
+    // Jika operasi gagal, maka fungsi akan melempar eror.
     if (response.statusCode != 200) throw Exception('Gagal menghapus');
   }
 }
